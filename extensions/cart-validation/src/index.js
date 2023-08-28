4b13bc54d21948c29de5f3d015aa7a64052785d6
Jeremiah Abalos <jeremiah.abalos@shopify.com>
2023-08-28 14:49:31 +0800

    demo of cart validation Shopify Functions API

---
+++ extensions/cart-validation/src/index.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/**
+ * @typedef {import("../generated/api").InputQuery} InputQuery
+ * @typedef {import("../generated/api").FunctionResult} FunctionResult
+ */
+
+export default /**
+ * @param {InputQuery} input
+ * @returns {FunctionResult}
+ */
+(input) => {
+  const combinableLines = input.cart.lines.filter(({ merchandise }) => merchandise.product.tagged === true);
+  const uncombinableLines = input.cart.lines.filter(({ merchandise }) => merchandise.product.tagged === false);
+  const combinationErrors = (combinableLines.length > 0 && uncombinableLines.length > 0) ? {
+    localizedMessage: "Not possible to combine tagged and untagged products",
+    target: "cart",
+  } : null;
+
+  const errors = input.cart.lines
+    .filter(({ merchandise, quantity }) => (merchandise.product.tagged === true && quantity > 1))
+    .map(({merchandise}) => ({
+      localizedMessage: `Not possible to order more than one of ${merchandise.product.title}`,
+      target: "cart",
+    }));
+
+  if (combinationErrors !== null) errors.push(combinationErrors);
+
+  return {
+    errors
+  }
+};
---
+++ extensions/cart-validation/src/index.test.js
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest';
+import validateCart from './index';
+
+/**
+ * @typedef {import("../generated/api").FunctionResult} FunctionResult
+ */
